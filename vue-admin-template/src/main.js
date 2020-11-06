b99187821af27b3dc3a823b8dea8d6d9461bba5e
lei <lei006@qq.com>
2020-11-06 10:33:53 +0800

    add BootstrapVue   inspinia 希望可以改比较好的后台

--- vue-admin-template/src/main.js
+++ vue-admin-template/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+//import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
+
 
 import App from './App'
 import store from './store'
@@ -14,6 +16,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+
 
 /**
  * If you don't want to use mock-server
@@ -29,9 +32,24 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+import '@/styles/animate.css' // global css
+import '@/styles/inspinia.css' // global css
+
 
 Vue.config.productionTip = false
 
